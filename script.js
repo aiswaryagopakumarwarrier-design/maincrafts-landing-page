a6b9f4287ee29c727f303803873b64ae95ecbe0d
aiswaryagopakumarwarrier-design <aiswaryagopakumarwarrier@gmail.com>
2026-06-02 14:49:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,47 @@
 const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.querySelector(".nav-links");
 
-menuBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
+if(menuBtn){
+  menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+}
+
+// CONTACT FORM VALIDATION
+
+const form = document.getElementById("contactForm");
+
+if(form){
+
+  form.addEventListener("submit", function(e){
+
+    const name =
+    document.getElementById("name").value.trim();
+
+    const email =
+    document.getElementById("email").value.trim();
+
+    const message =
+    document.getElementById("message").value.trim();
+
+    if(name === "" ||
+       email === "" ||
+       message === ""){
+
+      alert("Please fill all fields");
+      e.preventDefault();
+      return;
+    }
+
+    if(!email.includes("@")){
+
+      alert("Please enter a valid email address");
+      e.preventDefault();
+      return;
+    }
+
+    alert("Form submitted successfully!");
+
+  });
+
+}
